b6e535ba11c385baadc29c17c500f579bebe9fba
hippalus <habiphakan.isler@gmail.com>
2020-02-24 10:56:37 +0300

    first commit

---
+++ src/main/java/com/elasticsearch/practices/ElasticsearchPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elasticsearch/practices/ElasticsearchPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
